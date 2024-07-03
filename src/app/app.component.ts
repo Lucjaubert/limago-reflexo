5d1bf1d1382eb5a52e63be7dd1979451bd0122e8
luc_jaubert <j.luc@obvy-app.com>
2024-07-03 17:30:20 +0200

    Initialisation du projet avec README

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
+})
+export class AppComponent {
+  title = 'limago-reflexo';
+}
