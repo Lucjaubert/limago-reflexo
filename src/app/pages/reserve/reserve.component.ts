e0ab9ba7f15de36aedb8e1ffc1d8865a5277a372
luc_jaubert <j.luc@obvy-app.com>
2024-07-15 11:14:30 +0200

    Prepare for Angular update

--- src/app/pages/reserve/reserve.component.ts
+++ src/app/pages/reserve/reserve.component.ts
@@ -11,11 +11,13 @@ import { RouterModule } from '@angular/router';
   templateUrl: './reserve.component.html',
   styleUrls: ['./reserve.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, NgbModule],
 })
 export class ReserveComponent implements OnInit {
   reserveData$: Observable<ReserveData[] | null>;
   activeSection: string = 'reflexology_plantar';
+  selectedSessionTitle: string;
+  selectedSessionContent: string;
 
   constructor(private wpService: WordpressService) {
     this.reserveData$ = this.wpService.getReservation().pipe(
@@ -24,11 +26,36 @@ export class ReserveComponent implements OnInit {
         return of(null);
       })
     );
+    this.selectedSessionTitle = '';
+    this.selectedSessionContent = '';
   }
 
   ngOnInit(): void {}
 
   setActiveSection(section: string): void {
     this.activeSection = section;
+  }
+
+  setSelectedSession(title: string, content: string) {
+    this.selectedSessionTitle = title;
+    this.selectedSessionContent = content;
+  }  
+
+  prepareMailToLink() {
+    const subject = encodeURIComponent(`Réservation pour ${this.selectedSessionTitle}`);
+    const body = encodeURIComponent(
+      `Bonjour Magali,\n\n` +
+      `Je souhaite réserver la séance suivante le [écrivez la date souhaitée] à [écrivez l'heure souhaitée]:\n\n` +
+      `- ${this.selectedSessionTitle}\n\n` +
+      `${this.selectedSessionContent}\n\n` +
+      `Merci de me confirmer par mail ou bien de m'appeler au [écrivez votre numéro de portable].\n\n` +
+      `Cordialement,\n\n` +
+      `Votre Nom et Prénom`
+    );
+    return `mailto:limago.reflexo@gmail.com?subject=${subject}&body=${body}`;
+  }
+
+  get mailToLink(): string {
+    return this.prepareMailToLink();
   }
 }
