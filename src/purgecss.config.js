dc3988cc01f1fceb19713e96861bda786d5e0448
luc_jaubert <j.luc@obvy-app.com>
2024-10-22 09:36:39 +0200

    add css details

--- src/purgecss.config.js
+++ src/purgecss.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-    content: ['./src/**/*.html', './src/**/*.ts'],
-    css: ['./src/styles/styles.css'],
-    output: './dist/limago-reflexo/styles/',
-  };
-  
-  
+  content: [
+    './src/**/*.html',
+    './src/**/*.ts',
+  ],
+  css: ['./src/styles/styles.scss'],
+  safelist: {
+    standard: [],
+    deep: [],
+    greedy: [],
+  }
+}
+
   
